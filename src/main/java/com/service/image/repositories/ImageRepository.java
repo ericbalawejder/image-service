619db6f9de520b95449be32fed846e5a61651cea
Eric Balawejder <eric.balawejder@protonmail.com>
2022-02-21 22:21:27 -0500

    Removed unused method.

--- src/main/java/com/service/image/repositories/ImageRepository.java
+++ src/main/java/com/service/image/repositories/ImageRepository.java
@@ -1,14 +1,12 @@
 package com.service.image.repositories;
 
-import java.util.Optional;
-
 import com.service.image.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     Optional<Image> findByName(String name);
-
-    Optional<Image> findByHash(String hash);
 
 }

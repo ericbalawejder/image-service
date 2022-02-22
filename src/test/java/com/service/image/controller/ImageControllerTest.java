34498eeea922e6376a446f7c943b8cda768bd644
Eric Balawejder <eric.balawejder@protonmail.com>
2022-02-22 14:18:38 -0500

    Refactored controller tests to combine separate classes.

--- src/test/java/com/service/image/controller/ImageControllerTest.java
+++ src/test/java/com/service/image/controller/ImageControllerTest.java
@@ -1,7 +1,7 @@
 package com.service.image.controller;
 
 import com.service.image.entities.Image;
-import com.service.image.repositories.ImageRepository;
+import com.service.image.response.ImageUploadResponse;
 import com.service.image.util.ImageUtility;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,23 +9,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.Optional;
-
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ImageController.class)
-class ImageControllerTest {
+public class ImageControllerTest {
 
     @MockBean
-    private ImageRepository imageRepository;
+    private ImageController imageController;
 
     @Autowired
     private MockMvc mockMvc;
@@ -35,7 +38,7 @@ class ImageControllerTest {
     @BeforeEach
     void setUp() {
         image = new Image(100_000_000_000L,
-                "sunset.jpeg",
+                "sky.jpeg",
                 "jpeg",
                 900_000_000_000L,
                 "hash",
@@ -43,8 +46,27 @@ class ImageControllerTest {
     }
 
     @Test
+    public void whenUploadingImageThenReturnResponse() throws Exception {
+        final MockMultipartFile imageFile = new MockMultipartFile("image", new byte[]{127});
+
+        final ResponseEntity<ImageUploadResponse> imageUploadResponse = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ImageUploadResponse("Image uploaded successfully: " + image.getName()));
+
+        when(imageController.uploadImage(imageFile)).thenReturn(imageUploadResponse);
+
+        final MvcResult mvcResult = mockMvc.perform(multipart("/upload/image")
+                        .file(imageFile)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
     public void givenImageGetImageDetails() throws Exception {
-        given(imageRepository.findByName("sunset.jpeg")).willReturn(Optional.ofNullable(image));
+        given(imageController.getImageDetails("sky.jpeg")).willReturn(image);
 
         final MvcResult mvcResult = mockMvc.perform(get("/get/image/info/" + image.getName()))
                 .andExpect(status().isOk())

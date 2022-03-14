d4e8de09ae6eb7235126b61727cde0e78be5e05a
Eric Balawejder <eric.balawejder@protonmail.com>
2022-03-14 16:23:56 -0400

    Added RepositoryRestResource mappings.

--- src/main/java/com/service/image/repositories/ImageRepository.java
+++ src/main/java/com/service/image/repositories/ImageRepository.java
@@ -2,11 +2,13 @@ package com.service.image.repositories;
 
 import com.service.image.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     Optional<Image> findByName(String name);
